9d3bcb5123be98fe79cf45f105d0a3d7ea9393e3
Abel Kurian <ajtkurian@gmail.com>
2016-05-01 21:41:13 -0400

    Add comments to puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,8 @@
+# Puppy class should be able to perform tricks
+# method - bark
+# 	output: Output string like "woof"
+# method - "roll over"
+# 	output string telling that the dog rollover
+# method - feed dog
+# 	input - food item to feed dog
+# 	output -  the dog thanks the person feeding it
