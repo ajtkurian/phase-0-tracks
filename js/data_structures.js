1f836cc72d90b5592ab99fbb2f20d2e6e30d45ea
Abel Kurian <ajtkurian@gmail.com>
2016-05-04 16:20:05 -0400

    Refactor code

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,13 +13,21 @@ for (var i = 0; i < colors.length; i++) {
 
 console.log(horses);
 
-function Car(Engine, Doors, isHatchback) {
+ffunction Car(Engine, Doors, isHatchback) {
   console.log("Our new car", this);
   
   // Add attributes to the car
   this.engine = Engine;
   this.doors = Doors;
-  this.isHatchback = isHatchback;
+  this.Hatchback = function() {
+    if (isHatchback) {
+      return console.log("This car is a hatchback.");
+       //return "This car is a hatchback"
+    } else {
+      return console.log("This car is not a hatchback.");
+       //return "This car is not a hatchback."
+    };};
+      
   // create function to speed up
   this.speedUp = function() {console.log("VROOM VROOM!"); };
   
@@ -27,26 +35,17 @@ function Car(Engine, Doors, isHatchback) {
   
 }
 
-var car = new Car("V6", 2, false)
+var car = new Car("V6", 2, false);
 
-console.log("The car's engine is a " + car.engine)
-console.log("The car has " + car.doors + " doors.")
-if (car.isHatchback) {
-  console.log("This car is a hatchback.");
-}
-else {
-  console.log("This car is not a hatchback.");
-};
+console.log("The car's engine is a " + car.engine);
+console.log("The car has " + car.doors + " doors.");
+car.Hatchback();
 
-console.log("-------")
+console.log("-------");
 
-var anothercar = new Car("V8", 4, true)
+var anothercar = new Car("V8", 4, true);
 
-console.log("The car's engine is a " + anothercar.engine)
-console.log("The car has " + anothercar.doors + " doors.")
-if (anothercar.isHatchback) {
-  console.log("This car is a hatchback.");
-}
-else {
-  console.log("This car is not a hatchback.");
-};
+console.log("The car's engine is a " + anothercar.engine);
+console.log("The car has " + anothercar.doors + " doors.");
+anothercar.Hatchback();
+
